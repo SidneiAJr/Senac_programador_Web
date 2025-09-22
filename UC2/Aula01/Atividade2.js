5199366d1e59145ce3f8c74e6d0d1b6a8492b3ed
Sid Jr <sidalbertok@gmail.com>
2025-09-22 10:17:11 -0300

    Create Atividade2.js

---
+++ UC2/Aula01/Atividade2.js
@@ -0,0 +1,31 @@
+// Basico de JS(DeNovo)
+let n1 = 5;
+let n2 = 5;
+let soma = n1+n2;
+console.log(soma);
+
+// Basico de JS(If) 
+if(n1<=n2){
+    console.log(`Nota ${n1}`)
+}else{
+    console.log(`Nota ${n2}`)
+}
+
+//Basico de Loop
+let i = 0;
+while (i<=10){
+    i++
+    console.log(`Incrementando ${i}`)
+}
+
+//Basico de Loop For
+for(let i=0; i<=10; i++){
+     console.log(`Incrementando ${i}`)
+}
+
+//Basico de Array 
+let array = [1,2,3,4,5]
+let ar2 = array.push(6)
+for(let i=0; i<array.length; i++){
+    console.log(`Array Contado ${array}`)
+}
