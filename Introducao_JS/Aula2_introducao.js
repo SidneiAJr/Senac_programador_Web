b01a7d904e135e0a6b481c96f6dcfe7483def12e
Sid Jr <sidalbertok@gmail.com>
2025-11-18 10:31:08 -0300

    Update Aula2_introducao.js

--- Introducao_JS/Aula2_introducao.js
+++ Introducao_JS/Aula2_introducao.js
@@ -1,1 +1,15 @@
+function arrays() {
+    let array = ["Maça", "Motor", 20, 30]
+    array.push("Pedro")
+    console.log(array)
+    array.splice(1, 2, "Jaguara")
+    console.log(array)
+    array.unshift("Pedro mesa")
+    console.log(array)
+    array.shift(1)
+    console.log(array)
+    array.pop("pedro jr")
+    console.log(array)
 
+}
+arrays()
