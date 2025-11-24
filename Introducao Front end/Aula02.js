9aaecdd093c18afe8921e2859bedd357e714fc78
Sid Jr <sidalbertok@gmail.com>
2025-11-24 11:39:21 -0300

    Create Aula02.js

---
+++ Introducao Front end/Aula02.js
@@ -0,0 +1,10 @@
+function esconder(){
+    let documento = document.getElementById("info");
+
+    if(documento.style.display==="none"){
+     documento.style.display = "block"
+    }else{
+        documento.style.display="none"
+    }
+
+}
