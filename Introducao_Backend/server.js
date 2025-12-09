76ba512f7376527931ac0c9d6c64a96367439525
Sid Jr <sidalbertok@gmail.com>
2025-12-09 11:01:56 -0300

    Update server.js

--- Introducao_Backend/server.js
+++ Introducao_Backend/server.js
@@ -5,7 +5,17 @@ const express = require('express')
 // Sempre que eu quiser precisar acessar um metodo
 const app = express()
 
+//expres.sjon()tambem e um metodo da biblioteca dos express que indica que nosso codigo vai ler arquivos formato json
 app.use(express.json())
+
+//Define a porta do servidor
+const PORT = 3000
+
+// Aqui vamo definir rotas
+app.get('/pegar',(req,res)=>{
+    res.send('Ola Mundo');
+})
+
 
 
 
