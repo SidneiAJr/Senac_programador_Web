a371049829dfb51855ea14fc4290c310e333e86c
Sid Jr <sidalbertok@gmail.com>
2025-12-09 10:50:24 -0300

    Update server.js

--- Introducao_Backend/server.js
+++ Introducao_Backend/server.js
@@ -1,2 +1,13 @@
 // Importando Biblioteca eu uso express.motodo()
 const express = require('express')
+
+// Criar uma instancia do express
+// Sempre que eu quiser precisar acessar um metodo
+const app = express()
+
+app.use(express.json())
+
+
+
+
+
