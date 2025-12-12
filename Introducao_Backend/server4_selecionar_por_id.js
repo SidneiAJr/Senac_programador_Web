8afaa02cf22a3d9488ec57844c801f937323cbed
Sid Jr <sidalbertok@gmail.com>
2025-12-12 09:55:24 -0300

    Update server4_selecionar_por_id.js

--- Introducao_Backend/server4_selecionar_por_id.js
+++ Introducao_Backend/server4_selecionar_por_id.js
@@ -59,12 +59,11 @@ app.get('/ler/:id',(req,res)=>{
     // Pegue atravez do parametro da requesição
     const {id}= req.params
     const leitura = "SELECT * from usuarios where id = ?";
-     connection.query(leitura,(erro,resultado)=>{
+     connection.query(leitura,[id],(erro,resultado)=>{
          if(erro){
           return res.status(500).send("Erro | Leitura nâo Realizada")
          }
          res.status(200).json(resultado)
-
     })
 })
 
