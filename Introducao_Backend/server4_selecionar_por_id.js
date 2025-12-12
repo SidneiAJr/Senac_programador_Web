4333bf2f86b9f6753507236eb2fd5fbcb64d01b2
Sid Jr <sidalbertok@gmail.com>
2025-12-12 09:43:42 -0300

    Create server4_selecionar_por_id.js

---
+++ Introducao_Backend/server4_selecionar_por_id.js
@@ -0,0 +1,118 @@
+/*
+E o script do nosso servidor
+Nele vamos disctutir as rotas e o que cada uma delas faz
+nos tambem vamo nos conectar com nosso banco de dados
+*/
+
+const express = require('express')
+
+// Importando a biblioteca do mysql2 que tem funções para se conctar a interagir com meu banco dados
+const mysql = require('mysql2')
+
+const app = express()
+
+// Nosso servidor vai nos conectar
+app.use(express.json())
+
+const connection = mysql.createConnection({
+    host: 'localhost', // Host
+    port: 3306, // Porta
+    user : 'root', // usuario
+    password : 'root', // senha
+    database: 'banco_teste' // Nome Banco
+})
+
+// Conecta usando as informações que passamos acima
+connection.connect()
+
+/*
+==================================
+============ROTAS=================
+// Rotas para criar um usuario
+*/
+//Inserção de Informação
+app.post('/usuarios',(req,res)=>{
+    const {nome,email}=req.body
+    const comandobanco = "INSERT INTO usuarios (nome,email) values(?,?)"
+    // FUnção que me permite executar um comando de banco de dados
+    connection.query(comandobanco,[nome,email],(erro)=>{
+         if(erro){
+          return res.status(500).send("Erro ao adicionar usuario!")
+         }
+         return res.status(201).send("Usuario Adicionado com sucesso!")
+    })
+})
+// Seleciona tudo do banco
+app.get('/ler',(req,res)=>{
+    const leitura = "SELECT * FROM banco_teste.usuarios";
+     connection.query(leitura,(erro,resultado)=>{
+         if(erro){
+          return res.status(500).send("Erro | Leitura nâo Realizada")
+         }
+         res.status(200).json(resultado)
+
+    })
+})
+
+// Selecionando Pelo ID & Retorna de um usuario especifico
+app.get('/ler/:id',(req,res)=>{
+    // Pegue atravez do parametro da requesição
+    const {id}= req.params
+    const leitura = "SELECT * from usuarios where id = ?";
+     connection.query(leitura,(erro,resultado)=>{
+         if(erro){
+          return res.status(500).send("Erro | Leitura nâo Realizada")
+         }
+         res.status(200).json(resultado)
+
+    })
+})
+
+
+
+
+
+// Atualiza as informações
+app.get('/at',(req,res)=>{
+    const atualizao = "";
+     connection.query(atualizao,(erro,resultado)=>{
+         if(erro){
+          return res.status(500).send("Erro | Leitura nâo Realizada")
+         }
+         res.status(200).json(resultado)
+
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 3000
+app.listen(port,()=>{
+    console.log(`Servidor Rodando localhost:${port}`)
+})
+
