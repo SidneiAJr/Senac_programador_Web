6b956d0c3327ea9fd87639554498c903fd6dbbb9
Sid Jr <sidalbertok@gmail.com>
2025-12-15 14:22:01 -0300

    Update Banco_sql_atividade.sql

--- Atividade Backend/Banco_sql_atividade.sql
+++ Atividade Backend/Banco_sql_atividade.sql
@@ -7,3 +7,17 @@ nome_pokemon varchar(255) not null,
 tipo_pokemon varchar(255) not null,
 tem_evolucao enum ("sim","não")
 );
+
+-- Inserir dados na tabela pokemons
+INSERT INTO pokemons (nome_pokemon, tipo_pokemon, tem_evolucao) 
+VALUES 
+    ('Bulbasaur', 'Grama/Poison', 'sim'),
+    ('Charmander', 'Fogo', 'sim'),
+    ('Squirtle', 'Água', 'sim'),
+    ('Pikachu', 'Elétrico', 'sim'),
+    ('Eevee', 'Normal', 'sim'),
+    ('Jigglypuff', 'Normal/Fada', 'sim'),
+    ('Meowth', 'Normal', 'não'),
+    ('Mankey', 'Lutador', 'sim'),
+    ('Zubat', 'Venenoso/Voador', 'sim'),
+    ('Snorlax', 'Normal', 'não');
