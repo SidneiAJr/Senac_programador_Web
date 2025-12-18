435e0b07dbb711d6dc07336eb4d2be6baa8817c3
Sid Jr <sidalbertok@gmail.com>
2025-12-18 11:50:10 -0300

    Update calc1.js

--- PI Não Lembro/calc1.js
+++ PI Não Lembro/calc1.js
@@ -2,24 +2,25 @@ function calcular(){
     const valorsalariobruto = document.getElementById('salario').value
     const descontosvt = valorsalariobruto*0.06
     const salarioliquido = valorsalariobruto-descontosvt
-    /* Tabela de IR
-    | Faixa de Salário Bruto (R$) | Alíquota | Parcela a Deduzir (R$) |
-| --------------------------- | -------- | ---------------------- |
-| Até 2.112,00                | Isento   | 0                      |
-| De 2.112,01 a 2.826,65      | 7,5%     | 158,40                 |
-| De 2.826,66 a 3.751,05      | 15%      | 370,40                 |
-| De 3.751,06 a 4.664,68      | 22,5%    | 651,73                 |
-| Acima de 4.664,68           | 27,5%    | 884,96                 |
+    const ir1 = 0.10;
+    const ir2 = 0.15;
+    const ir3 = 0.20;
+    const ir4 = 0.25;
 
-    */
-
-    const ir = valorsalariobruto
 
     if(valorsalariobruto<=4000){
         document.getElementById('res').innerHTML=`
         Salario R$ ${valorsalariobruto} Não há Imposto
         Salario Liquido R$ ${salarioliquido.toFixed(2)}
         `
+    }else if(valorsalariobruto<2826.66 && valorsalariobruto>=2826.66){
+        const desc = valorsalariobruto*ir1
+        const desctotal = salarioliquido- desc
+        document.getElementById('res').innerHTML=`
+        Salario R$ ${valorsalariobruto} Não há Imposto
+        Salario Liquido R$ ${desctotal.toFixed(2)}
+        `
     }
+
 
 }
