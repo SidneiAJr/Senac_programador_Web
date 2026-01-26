59499744f0a79e5eac06633239f6e0908e231660
Sid Jr <sidalbertok@gmail.com>
2026-01-26 11:59:52 -0300

    Update server.js

--- Atividade Backend/Nova_Atividade_backend/server.js
+++ Atividade Backend/Nova_Atividade_backend/server.js
@@ -2,5 +2,6 @@ const express = require('express')
 const mysql = require('mysql2')
 const cors = require('cors')
 const Body = require('body-parser')
+const dot = require('dotenv')
 
-
+dot.config();
