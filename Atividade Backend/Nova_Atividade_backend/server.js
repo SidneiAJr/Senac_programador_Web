cd26565342d2187349765764b1fdda110a03c920
Sid Jr <sidalbertok@gmail.com>
2026-01-27 11:28:00 -0300

    Update server.js

--- Atividade Backend/Nova_Atividade_backend/server.js
+++ Atividade Backend/Nova_Atividade_backend/server.js
@@ -41,6 +41,17 @@ app.get('/users',(req,res)=>{
    })
 })
 
+app.get('/users:id',(req,res)=>{
+    const {id} = req.params;
+    const consulta_id = 'select * from usuarios where id = ?'
+    connection.query(consulta_id,(erro,resultado)=>{
+        if(erro){
+          return res.status(500).set("Erro ao Selecionar dados");  
+        }
+        return res.status(200).json(resultado[0]);
+    })
+})
+
 app.get('/Lista',(req,res)=>{
    const consulta_lista = `select count (*) from usuarios`;
    connection.query(consulta_lista,(erro,resultados)=>{
@@ -63,10 +74,11 @@ app.post('/Insert',(req,res)=>{
 })
 
 app.delete('/deletar/:id',(req,res)=>{
-    const {id} = req.params;
-    const deletar = `delete from usuarios where id =?`
-    connection.query(deletar,[id],(erro)=>{
+    const {id_usuario} = req.params;
+    const deletar = `delete from usuarios where id_usuario =?`
+    connection.query(deletar,[id_usuario],(erro)=>{
         if(erro){
+            console.error('Erro ao deletar:', erro);  // Log do erro
             return res.status(500).send("Erro ao Apagar Usuario!");
         }
         return res.status(200).send("Sucesso ao Deletar Usuario");
