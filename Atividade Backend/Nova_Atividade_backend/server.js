5ac3829046f4761e8c9baaa75e8769bd57769991
Sid Jr <sidalbertok@gmail.com>
2026-01-27 10:02:52 -0300

    Update server.js

--- Atividade Backend/Nova_Atividade_backend/server.js
+++ Atividade Backend/Nova_Atividade_backend/server.js
@@ -1,7 +1,36 @@
+// Imports de Libs | JS
 const express = require('express')
 const mysql = require('mysql2')
 const cors = require('cors')
 const Body = require('body-parser')
 const dot = require('dotenv')
+const bodyParser = require('body-parser')
+
+//Iniciando express
+const app = express();
+app.express(bodyParser.json());
+app.use(cors());
+
+// Conexção Banco
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'banco_trabalho'
+});
+connection.connect();
+
+app.post({});
+app.get({});
+app.put({});
+app.delete({});
+app.get({});
+
+
+
+
+
 
 dot.config();
+
