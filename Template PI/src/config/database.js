5f2f886cd9c5a87c124a2767e7c6ac125565375d
Sid Jr <sidalbertok@gmail.com>
2026-02-09 11:19:08 -0300

    Update database.js

--- Template PI/src/config/database.js
+++ Template PI/src/config/database.js
@@ -1,9 +1,5 @@
 import mysql from "mysql2";
-import bcrypt from "bcryptjs";
-import express from "express";
-import detoenv from "detoenv";
-import jsonweb from "jsonwebtoken";
-import cors from "cors";
+
 
 
 
