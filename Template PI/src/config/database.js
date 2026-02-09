02d5c2fc2340f8aa7484e253b5d69097112d7deb
Sid Jr <sidalbertok@gmail.com>
2026-02-09 11:27:12 -0300

    Update database.js

--- Template PI/src/config/database.js
+++ Template PI/src/config/database.js
@@ -1,4 +1,27 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
+import ratelimit from "rate-limit";
+
+import { DB_DATABASE } from "./env-config";
+import { DB_PORT } from "./env-config";
+import { DB_HOST } from "./env-config";
+import { DB_USER } from "./env-config";
+import { DB_Password } from "./env-config";
+
+export const conn = mysql.createPool({
+   host:DB_HOST,
+   port: parseInt(DB_PORT|| '3306'),
+   user: DB_USER,
+   password: DB_Password,
+   database:DB_DATABASE
+})
+
+
+
+
+
+
+
+
 
 
 
