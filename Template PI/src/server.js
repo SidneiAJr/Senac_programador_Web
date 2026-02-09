9ed8c8145a80abc97f85bd3cf01665744dccacbf
Sid Jr <sidalbertok@gmail.com>
2026-02-09 10:46:18 -0300

    Update server.js

--- Template PI/src/server.js
+++ Template PI/src/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config()
 
 const app = express()
 const port = process.env.port || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cors())
+app.listen(port,()=>{
+    console.log(`Servidor Rodando: http://localhost${port}`)
+})
+
+
 
 
 
