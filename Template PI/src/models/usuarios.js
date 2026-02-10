c8098ca136e200b5e86a052e4bba824fe166d8ce
Sid Jr <sidalbertok@gmail.com>
2026-02-10 09:37:47 -0300

    Update usuarios.js

--- Template PI/src/models/usuarios.js
+++ Template PI/src/models/usuarios.js
@@ -1,9 +1,8 @@
  export class Usuario{
-    constructor(id,nome,idade,cpf,telefone,email){
+    constructor(id,nome,idade,telefone,email){
     this.id = id;
     this.nome = nome;
     this.idade = idade;
-    this.cpf = cpf;
     this.telefone = telefone;
     this.email = email;
     }
