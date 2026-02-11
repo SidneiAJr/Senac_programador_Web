73189afe9a07d7e89a2e7b133fc6623240e1286f
Sid Jr <sidalbertok@gmail.com>
2026-02-11 10:35:18 -0300

    Update user-controller.js

--- Template PI/src/controller/user-controller.js
+++ Template PI/src/controller/user-controller.js
@@ -1,3 +1,4 @@
+import { Usuario } from "../models/user.js";
 import { UsuarioRepository } from "../repositories/user-repository.js"
 export class UserController{
 
@@ -7,14 +8,26 @@ export class UserController{
     }
 
     static async findID(res,req){
-        const findbyID = parseInt(req.params.id);
-        const usuario = await UsuarioRepository.findID(id);
+        const ID = parseInt(req.params.id);
+        const usuario = await UsuarioRepository.findID(ID);
         if(!usuario){
        res.status(404).send('Usuario não Encontrado Erro!!!')
        return;
     }
        res.status(200).json(usuarios);
     }
+
+    static async register(req,res){
+        const {nome, idade, telefone, email} = req.body;
+
+        if(!nome || ! idade || !telefone || !email){
+            res.status(400).send("Falta Informação")
+            return
+        }
+        const Usuario = new Usuario(null,nome,idade,telefone,email)
+        const novoUsuario = await UsuarioRepository.insert(Usuario)
+    }
+
 
     
 }
