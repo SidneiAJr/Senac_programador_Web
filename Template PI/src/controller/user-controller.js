97b2082009a52e6688ef6bff5bade5e95e1e1984
Sid Jr <sidalbertok@gmail.com>
2026-02-12 11:54:42 -0300

    Update user-controller.js

--- Template PI/src/controller/user-controller.js
+++ Template PI/src/controller/user-controller.js
@@ -1,30 +1,40 @@
 import { Usuario } from "../models/user.js";
 import { UsuarioRepository } from "../repositories/user-repository.js"
+import { UserService } from "../services/user-services.js";
 export class UserController{
 
     static async index(req,res){
-       const usuarios = await UsuarioRepository.findAll();
-       res.status(200).json(usuarios);
+        try{
+         const usuarios = await UserService.exibirUsuarios();
+         res.status(200).json(usuarios)
+        }catch(error){
+         res.status(404).json({message: error.message})
+        }
     }
 
     static async findID(res,req){
-        const ID = parseInt(req.params.id);
-        const usuario = await UsuarioRepository.findID(ID);
-        if(!usuario){
-       res.status(404).send('Usuario não Encontrado Erro!!!')
-       return;
-    }
-       res.status(200).json(usuario);
+        const id = parseInt(req.params.id);
+       
+        try{
+            const usuario = await UserService.exibirUsuario(id);
+            res.status(200).json(usuario)
+        }catch(error){
+            let status = 400;
+            if(error.message.includes('Usuario Não Encontrado')){
+              status = 404
+            }
+            res.status(status).send(error.message)
+        }
     }
 
     static async register(req,res){
-        const {nome, idade, telefone, email} = req.body;
+        const {nome, idade, telefone, email,senha} = req.body;
 
-        if(!nome || ! idade || !telefone || !email){
+        if(!nome || ! idade || !telefone || !email || !senha){
             res.status(400).send("Falta Informação")
             return
         }
-        const usuario = new Usuario(null,nome,idade,telefone,email)
+        const usuario = new Usuario(null,nome,idade,telefone,email,senha)
         const novoUsuario = await UsuarioRepository.insert(usuario)
 
         res.status(201).json(novoUsuario)
