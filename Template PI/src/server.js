ce4bb3c6a67164610bbc18b2b8707cf3e5ad2648
Sid Jr <sidalbertok@gmail.com>
2026-02-18 10:48:59 -0300

    Update server.js

--- Template PI/src/server.js
+++ Template PI/src/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import usuarioRoutes from './routes/user-routes.js' 
 import {limiter} from "./config/rate-limit.js";
+import { erroMiddleware } from "./middlewares/error-middleware.js";
 
 dotenv.config()
 
@@ -14,13 +15,15 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors())
 app.use(limiter);
 
+app.use(usuarioRoutes)
+app.use(erroMiddleware)
+
 
 app.listen(port,()=>{
     console.log(`Servidor Rodando: http://localhost:${port}`)
 })
 
 
-app.use(usuarioRoutes)
 
 
 
