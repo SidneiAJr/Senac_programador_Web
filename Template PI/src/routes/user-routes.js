4b6d1210feda71783bae904df76edb1f176aa04d
Sid Jr <sidalbertok@gmail.com>
2026-02-18 10:59:52 -0300

    Update user-routes.js

--- Template PI/src/routes/user-routes.js
+++ Template PI/src/routes/user-routes.js
@@ -5,6 +5,10 @@ const router = Router();
 router.get('/user',UserController.index);
 router.get('/user/:id',UserController.findID);
 router.post('/user',UserController.register)
+router.delete('/user/:id',UserController.delete);
+router.put('/user/:id',UserController.update);
+router.post('/user/login',UserController.login);
+
 
 export default router
 
