81b2feb52adb83b059a40c541fc4be84e49c97bc
Sid Jr <sidalbertok@gmail.com>
2026-02-19 11:54:41 -0300

    Update user-services.js

--- Template PI/src/services/user-services.js
+++ Template PI/src/services/user-services.js
@@ -5,122 +5,122 @@ import { UsuarioRepository } from "../repositories/user-repository.js";
 import bcrypt, { genSalt } from "bcryptjs";
 import jwt from 'jsonwebtoken'
 
-export class UserService{
-    static async exibirUsuarios(){
-        const usuario =  UsuarioRepository.findAll()
+export class UserService {
+   static async exibirUsuarios() {
+      const usuario = UsuarioRepository.findAll()
 
-        if(usuario.length ===0){
-             throw new AppError('Nenhum Usuario Encontrado',404);
-        }
-        return usuario
-    }
-
-    static async exibirUsuario(id){
-        if(isNaN(id)){
-           throw new AppError('Precisa ser um Numero!',400)
-        }
-         const usuario = await UsuarioRepository.findID(id)
-         
-        if(!usuario){
-           throw new AppError('Erro ao Buscar Dados!',404);
-        }
-    }
-    static async RegistrarUsuario(nome,idade ,telefone ,email, senha){
-         const emailExiste = await UsuarioRepository.findEmail(email);
-
-         if(!nome || ! idade || !telefone || !email || !senha){
-            throw new AppError("Todos os Campos são Obrigatorios",400)
-        }
-         if(emailExiste){
-            throw new AppError("Email Existe | Favor Usar Outro!",409)
-         }
-         
-        const senhaHash = await bcrypt.hash(senha,10);
-
-        const usuario = new Usuario(null,nome,idade,telefone,email,senhaHash)
-
-        const novousuario = await UsuarioRepository.insert(usuario)
-
-        return novousuario;
-         
-    }
-
-    static async login(email,senha){
-       if(!email || !senha){
-        throw new AppError('Email & Senha são Obrigatorios',400)
-       }
-       const usuario = await UsuarioRepository.findEmail(email);
-       if(!usuario){
-        throw new AppError('Credenciais Invalidas !',401);
-       }
-
-       const Senhachecar = await bcrypt.compare(senha,usuario.senha);
-
-       if(!Senhachecar){
-        throw new AppError('Credencias Invalidas',401)
-       }
-      
-       const token = jwt.sign(
-         {id:usuario.id, role: usuario.role}
-       )
-       JWT_Secret,
-       {expiresIn:'1h'}
-
-       return{token}
-    }
-
-    static async update(id,nome,idade ,telefone ,email, senha){
-      if(isNaN(id)){
-         throw new AppError('ID precisa ser um numero',400)
+      if (usuario.length === 0) {
+         throw new AppError('Nenhum Usuario Encontrado', 404);
       }
-      
-      if(!nome && !email && !senha && !idade && !telefone){
-          throw new AppError('É ncessario informar ao menos um campo para alteração',400)
+      return usuario
+   }
+
+   static async exibirUsuario(id) {
+      if (isNaN(id)) {
+         throw new AppError('Precisa ser um Numero!', 400)
+      }
+      const usuario = await UsuarioRepository.findID(id)
+
+      if (!usuario) {
+         throw new AppError('Erro ao Buscar Dados!', 404);
+      }
+   }
+   static async RegistrarUsuario(nome, idade, telefone, email, senha) {
+      const emailExiste = await UsuarioRepository.findEmail(email);
+
+      if (!nome || !idade || !telefone || !email || !senha) {
+         throw new AppError("Todos os Campos são Obrigatorios", 400)
+      }
+      if (emailExiste) {
+         throw new AppError("Email Existe | Favor Usar Outro!", 409)
+      }
+
+      const senhaHash = await bcrypt.hash(senha, 10);
+
+      const usuario = new Usuario(null, nome, idade, telefone, email, senhaHash)
+
+      const novousuario = await UsuarioRepository.insert(usuario)
+
+      return novousuario;
+
+   }
+
+   static async login(email, senha) {
+      if (!email || !senha) {
+         throw new AppError('Email & Senha são Obrigatorios', 400)
+      }
+      const usuario = await UsuarioRepository.findEmail(email);
+      if (!usuario) {
+         throw new AppError('Credenciais Invalidas !', 401);
+      }
+
+      const Senhachecar = await bcrypt.compare(senha, usuario.senha);
+
+      if (!Senhachecar) {
+         throw new AppError('Credencias Invalidas', 401)
+      }
+
+      const token = jwt.sign(
+         { id: usuario.id, role: usuario.role },
+         JWT_Secret,
+         { expiresIn: '1h' }
+      )
+
+      return { token }
+   }
+
+   static async update(id, nome, idade, telefone, email, senha) {
+      if (isNaN(id)) {
+         throw new AppError('ID precisa ser um numero', 400)
+      }
+
+      if (!nome && !email && !senha && !idade && !telefone) {
+         throw new AppError('É ncessario informar ao menos um campo para alteração', 400)
       }
 
       const usuarioExist = await UsuarioRepository.findID(parseInt(id))
 
-      if(!usuarioExist){
-         throw new AppError('Usuario não encontrado',404)
+      if (!usuarioExist) {
+         throw new AppError('Usuario não encontrado', 404)
       }
 
-      const emailExist = await UsuarioRepository.findEmail(email) 
-      if(emailExist){
-           throw new AppError('Ja existe esse email veio!',409)
+      const emailExist = await UsuarioRepository.findEmail(email)
+      if (emailExist) {
+         throw new AppError('Ja existe esse email veio!', 409)
       }
-      
+
 
       nome = nome ?? usuarioExist.nome
       email = email ?? usuarioExist.email
       senha = senha ?? usuarioExist.senha
 
-      const usuarioAtualizado = await UsuarioRepository.update(id,new Usuario(id,nome,email,senha))
+      const usuarioAtualizado = await UsuarioRepository.update(id, new Usuario(id, nome, email, senha))
 
-      if(!usuarioAtualizado){
-         throw new AppError('Erro ao Atualizar Usuario',500)
+      if (!usuarioAtualizado) {
+         throw new AppError('Erro ao Atualizar Usuario', 500)
       }
 
       return usuarioAtualizado
-    }
+   }
 
-    static async delete(id){
-       if(isNaN(id)){
-          throw new AppError('ID precisa ser um numero',400)
-       }
-       const usuario = await UsuarioRepository.findID(parseInt(id))
-       const deleted = await UsuarioRepository.deleteById(id)
+   static async delete(id) {
+      if (isNaN(id)) {
+         throw new AppError('ID precisa ser um numero', 400)
+      }
+      const usuario = await UsuarioRepository.findID(parseInt(id))
+      const deleted = await UsuarioRepository.deleteById(id)
 
-       if(!usuario || !deleted){
-         throw new AppError('Usuario Não encontrado! ',404)
-       }
+      if (!usuario || !deleted) {
+         throw new AppError('Usuario Não encontrado! ', 404)
+      }
 
-       return 
-
-       
-    }
+      return
 
 
-    
+   }
+
+
+
 
 
 
