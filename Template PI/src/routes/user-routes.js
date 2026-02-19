355bbbc2db7aab51e0b3ea05901ee4ff47b5301e
Sid Jr <sidalbertok@gmail.com>
2026-02-19 11:41:08 -0300

    Update user-routes.js

--- Template PI/src/routes/user-routes.js
+++ Template PI/src/routes/user-routes.js
@@ -5,7 +5,7 @@ import {authMiddleware} from '../middlewares/Auth-middleware.js'
 const router = Router();
 router.get('/user',authMiddleware,UserController.index);
 router.get('/user/:id',authMiddleware,UserController.findID);
-router.post('/user',UserController.register)
+router.post('/user/register',UserController.register)
 router.delete('/user/:id',authMiddleware,UserController.delete);
 router.put('/user/:id',authMiddleware,UserController.update);
 router.post('/user/login',UserController.login);
