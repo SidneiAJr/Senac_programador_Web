746814162aeadcdd06b203ba5b9d402118a124b7
Sid Jr <sidalbertok@gmail.com>
2026-02-20 10:35:19 -0300

    Update server.js

--- Template PI/src/server.js
+++ Template PI/src/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import usuarioRoutes from './routes/user-routes.js' 
-import {limiter} from "./config/rate-limit.js";
 import { erroMiddleware } from "./middlewares/error-middleware.js";
 
 dotenv.config()
@@ -13,7 +12,6 @@ const port = process.env.port || 3000
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors())
-app.use(limiter);
 
 app.use(usuarioRoutes)
 app.use(erroMiddleware)
